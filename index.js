8770cc1382e8210328fa642d5fb712c7f797bb33
Ali Haider <ali1500haider@gmail.com>
2024-05-08 13:13:40 +0500

    add variable file

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const updateFunction = require("./update")
+// const updateFunction = require("./update")
 
-const main = ()=>{
-  let a=1
-  let b=2
-  console.log("main function here")
-  const updateValue=(valueA,valueB)=>{
-    a=valueA
-    b=valueB
-  }
-  console.log("before update",a,b)
-  updateFunction(updateValue)
-  console.log("after update",a,b)
+// const main = ()=>{
+//   let a=1
+//   let b=2
+//   console.log("main function here")
+//   const updateValue=(valueA,valueB)=>{
+//     a=valueA
+//     b=valueB
+//   }
+//   console.log("before update",a,b)
+//   updateFunction(updateValue)
+//   console.log("after update",a,b)
 
-}
+// }
 
-main()
+// main()
+
+const arr= [1,2,34,5]
+
+const newArr = arr.map((item,index,arr)=>{
+  console.log(item,"item")
+  console.log(index,"index")
+  console.log(arr,"arr")
+   return arr[index] = item*2
+})
+
+console.log(newArr)
---
+++ variable.js
@@ -0,0 +1,3 @@
+let firstVar = 200
+let secondVar = 500
+console.log(firstVar,secondVar)
